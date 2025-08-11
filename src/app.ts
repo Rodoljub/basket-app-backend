2e99c37d010185bcc45e79010631a65b9ae87118
Rodoljub Tanasic <rodoljub.tanasic@outlook.com>
2025-08-11 12:16:41 +0200

    temp

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,18 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
-  origin: '*'
-  // origin: process.env.CORS_ORIGIN, // frontend origin
-//   credentials: true               // if you use cookies/auth
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(cors({
+//   origin: '*'
+//   // origin: process.env.CORS_ORIGIN, // frontend origin
+// //   credentials: true               // if you use cookies/auth
+// }));
 
 app.use(express.json());
 
